5fad9d01bc74b7fe550ea36a95bbbca3f1cdf5c6
Krishna Acondy <kc.acondy@outlook.com>
2020-07-29 22:52:17 +0100

    chore(sasjs-tests): remove unnecessary code, bump test framework version

--- sasjs-tests/src/App.tsx
+++ sasjs-tests/src/App.tsx
@@ -5,17 +5,13 @@ import { sendArrTests, sendObjTests } from "./testSuites/RequestData";
 import { specialCaseTests } from "./testSuites/SpecialCases";
 import { sasjsRequestTests } from "./testSuites/SasjsRequests";
 import "@sasjs/test-framework/dist/index.css";
-import "./App.scss";
 
 const App = (): ReactElement<{}> => {
-  const [appLoc, setAppLoc] = useState("");
-  const [debug, setDebug] = useState(false);
   const { adapter, config } = useContext(AppContext);
   const [testSuites, setTestSuites] = useState<TestSuite[]>([]);
 
   useEffect(() => {
     if (adapter) {
-      adapter.setDebugState(debug);
       setTestSuites([
         basicTests(adapter, config.userName, config.password),
         sendArrTests(adapter),
@@ -24,44 +20,10 @@ const App = (): ReactElement<{}> => {
         sasjsRequestTests(adapter),
       ]);
     }
-  }, [debug, adapter, config]);
-
-  useEffect(() => {
-    if (appLoc && adapter) {
-      adapter.setSASjsConfig({ ...adapter.getSasjsConfig(), appLoc });
-    }
-  }, [appLoc, adapter]);
-
-  useEffect(() => {
-    setAppLoc(adapter.getSasjsConfig().appLoc);
-  }, [adapter]);
+  }, [adapter, config]);
 
   return (
     <div className="app">
-      <div className="controls">
-        <div className="row">
-          <label>Debug</label>
-          <div className="debug-toggle">
-            <label className="switch">
-              <input
-                type="checkbox"
-                onChange={(e) => setDebug(e.target.checked)}
-              />
-              <span className="knob"></span>
-            </label>
-          </div>
-        </div>
-        <div className="row app-loc">
-          <label>App Loc</label>
-          <input
-            type="text"
-            className="app-loc-input"
-            value={appLoc}
-            onChange={(e) => setAppLoc(e.target.value)}
-            placeholder="AppLoc"
-          />
-        </div>
-      </div>
       {adapter && testSuites && <TestSuiteRunner testSuites={testSuites} />}
     </div>
   );
