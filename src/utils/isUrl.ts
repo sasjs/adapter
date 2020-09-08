3da5aba62a4cce911f34e15449e0367257bd8c44
Yury Shkoda <shkoda.yuri@gmail.com>
2020-09-08 16:07:02 +0300

    fix: removed unnecessary check for www subdomain

--- src/utils/isUrl.ts
+++ src/utils/isUrl.ts
@@ -1,6 +1,6 @@
 export const isUrl = (url: string): boolean => {
   const pattern = new RegExp(
-    '^(http://www.|https://www.|http://|https://)[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$',
+    '^(http://|https://)[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$',
     'gi'
   )
 
