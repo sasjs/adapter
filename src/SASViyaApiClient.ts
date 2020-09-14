afec5609521a62c3ac0d45a35599831aa1874fb3
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-09-14 16:26:27 +0200

    fix: JES API error handling if job does not exist

--- src/SASViyaApiClient.ts
+++ src/SASViyaApiClient.ts
@@ -1001,9 +1001,21 @@ export class SASViyaApiClient {
 
     if (allJobsInFolder) {
       const jobSpec = allJobsInFolder.find((j: Job) => j.name === jobName)
+
+      if (!jobSpec) {
+        console.error('Job was not found.')
+        throw new Error('Job was not found.')
+      }
+
       const jobDefinitionLink = jobSpec?.links.find(
         (l) => l.rel === 'getResource'
       )?.href
+
+      if (!jobDefinitionLink) {
+        console.error('Job definition URI was not found.')
+        throw new Error('Job definition URI was not found.')
+      }
+
       const requestInfo: any = {
         method: 'GET'
       }
