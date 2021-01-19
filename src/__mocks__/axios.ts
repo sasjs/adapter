4196901e015a99b8745b3b3a4661d9d9906cae4d
Krishna Acondy <kc.acondy@outlook.com>
2021-01-19 08:17:42 +0000

    chore(*): remove comment

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -2,7 +2,6 @@ import { AxiosStatic } from 'axios'
 
 const mockAxios = jest.genMockFromModule('axios') as AxiosStatic
 
-// this is the key to fix the axios.create() undefined error!
 mockAxios.create = jest.fn(() => mockAxios)
 
 export default mockAxios
