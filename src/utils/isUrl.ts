48442f77697d6fd12cc1556ee522503da6709bc0
Yury Shkoda <shkoda.yuri@gmail.com>
2021-04-21 08:11:13 +0300

    fix(utility): improved 'isUrl' utility

--- src/utils/isUrl.ts
+++ src/utils/isUrl.ts
@@ -3,14 +3,11 @@
  * @param url - string to check.
  */
 export const isUrl = (url: string): boolean => {
-  const pattern = new RegExp(
-    '^(http://|https://)[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$',
-    'gi'
-  )
+  try {
+    const validUrl = new URL(url)
+  } catch (_) {
+    return false
+  }
 
-  if (pattern.test(url)) return true
-  else
-    throw new Error(
-      `'${url}' is not a valid url. An example of a valid url is 'http://valid-url.com'.`
-    )
+  return true
 }
