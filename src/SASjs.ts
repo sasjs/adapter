919c83c143baf2109c4aed9e26b58fa91181983b
sabhas <sabirbk06@gmail.com>
2021-06-09 16:40:29 +0500

    chore: lint fixes

--- src/SASjs.ts
+++ src/SASjs.ts
@@ -599,7 +599,9 @@ export default class SASjs {
    * @param data a json object contains one or more table, it can also be null
    * @returns a object which contains two attributes: 1) status: boolean, 2) msg: string
    */
-  private validateInput(data: { [key: string]: any }): {
+  private validateInput(data: {
+    [key: string]: any
+  }): {
     status: boolean
     msg: string
   } {
