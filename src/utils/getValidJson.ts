0bc69401e5a2e3b4015058b64cbcf60e30dbde73
sabhas <sabirbk06@gmail.com>
2021-08-18 00:02:48 +0500

    chore: refactor code for getValidJson function

--- src/utils/getValidJson.ts
+++ src/utils/getValidJson.ts
@@ -1,16 +1,19 @@
+import { JsonParseArrayError, InvalidJsonError } from '../types/errors'
+
 /**
  * if string passed then parse the string to json else if throw error for all other types unless it is not a valid json object.
  * @param str - string to check.
  */
 export const getValidJson = (str: string | object) => {
   try {
-    if (Array.isArray(str)) {
-      throw new Error('Can not parse array object to json.')
-    }
+    const arrayErrorMessage = 'Can not parse array object to json.'
+    if (Array.isArray(str)) throw new JsonParseArrayError(arrayErrorMessage)
+
     if (typeof str === 'object') return str
 
     return JSON.parse(str)
   } catch (e) {
-    throw new Error('Invalid JSON response.')
+    if (e instanceof JsonParseArrayError) throw e
+    throw new InvalidJsonError()
   }
 }
