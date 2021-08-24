77c4c473c10b43af63441a6a1f237d85cdf56f1a
sabhas <sabirbk06@gmail.com>
2021-08-24 11:51:18 +0500

    chore: address requested changes

--- checkNodeVersion.js
+++ checkNodeVersion.js
@@ -1,14 +1,9 @@
 const result = process.versions
 if (result && result.node) {
-  if (parseInt(result.node) >= 14) {
-    console.log(
-      '\x1b[32m%s\x1b[0m',
-      `Good to Go with your Node Version: ${result.node}`
-    )
-  } else {
+  if (parseInt(result.node) < 14) {
     console.log(
       '\x1b[31m%s\x1b[0m',
-      `Package installation(npm install) failed due to Node Version,\nPlease install and use Node Version >=14\nYour current Node Version is: ${result.node}`
+      `Process failed due to Node Version,\nPlease install and use Node Version >= 14\nYour current Node Version is: ${result.node}`
     )
     process.exit(1)
   }
