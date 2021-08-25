33e7564e8f06567ea4f3fc14e6eda1562d3e7bd8
sabhas <sabirbk06@gmail.com>
2021-08-25 21:26:49 +0500

    chore: add cross icon at the start of error message

--- checkNodeVersion.js
+++ checkNodeVersion.js
@@ -3,7 +3,7 @@ if (result && result.node) {
   if (parseInt(result.node) < 14) {
     console.log(
       '\x1b[31m%s\x1b[0m',
-      `Process failed due to Node Version,\nPlease install and use Node Version >= 14\nYour current Node Version is: ${result.node}`
+      `❌ Process failed due to Node Version,\nPlease install and use Node Version >= 14\nYour current Node Version is: ${result.node}`
     )
     process.exit(1)
   }
