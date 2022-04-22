54516665bf28213f10f8502bf188e8490ebbf8f4
Mihajlo <mihajlo.medjedovic@outlook.com>
2022-04-22 11:54:59 +0200

    chore: string escaping

--- src/utils/convertToCsv.ts
+++ src/utils/convertToCsv.ts
@@ -134,7 +134,7 @@ export const convertToCSV = (
         if (value && !isSpecialMissing(value)) {
           console.log(`🤖[value]🤖`, value)
           throw new Error(
-            'A Special missing value can only be a single character from \'A\' to \'Z\', \'_\', \'.[a-z]\', \'._\''
+            `A Special missing value can only be a single character from 'A' to 'Z', '_', '.[a-z]', '._'`
           )
         }
 
