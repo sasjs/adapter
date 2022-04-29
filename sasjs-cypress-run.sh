eb296bf93ce716dff20145b3f7abc7084733af43
Mihajlo <mihajlo.medjedovic@outlook.com>
2022-04-29 14:53:20 +0200

    chore: ci fix

--- sasjs-cypress-run.sh
+++ sasjs-cypress-run.sh
@@ -3,7 +3,7 @@
 if npm run cy:run -- --spec "cypress/integration/sasjs.tests.ts" ; then
     echo "Cypress sasjs testing passed!"
 else
-    curl -X POST --header "Content-Type:application/json" --data '{"username":"GitHub CI - Adapter SASJS-TESTS (FAIL)", "content":"Publish of a @sasjs/adapter has been canceled because of the failing sasjs-tests!", "avatar_url":"https://i.ibb.co/Lpk7Xvq/error-outline.png"}' https://discord.com/api/webhooks/969275140087111690/NGowOKrzk-ejP4gkOB-N4-cCPoWFPIux9OEvMGqX73jJxXQqD7H9AhCL_dpxYBJrP0Oi
+    curl -X POST --header "Content-Type:application/json" --data '{"username":"GitHub CI - Adapter SASJS-TESTS (FAIL)", "content":"Automated sasjs-tests failed on the @sasjs/adapter PR on following link.\n'$2'", "avatar_url":"https://i.ibb.co/Lpk7Xvq/error-outline.png"}' $1
     # curl -X POST --data-urlencode "payload={\"channel\":\"#sasjs\", \"username\":\"GitHub CI\", \"text\":\"Publish of a @sasjs/adapter has been canceled because of the failing sasjs-tests!\", \"icon_emoji\":\":warning:\"}" $1
     echo "Cypress sasjs testing failed!"
     exit 1
