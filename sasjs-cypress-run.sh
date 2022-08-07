abe95f5432ca9a28f84345b2e5ccfe44b279e085
munja <allnbwe@gmail.com>
2022-08-07 13:47:50 +0100

    chore: moving github docs to .github folder and migrating discord hook to matrix

--- sasjs-cypress-run.sh
+++ sasjs-cypress-run.sh
@@ -3,8 +3,7 @@
 if npm run cy:run -- --spec "cypress/integration/sasjs.tests.ts" ; then
     echo "Cypress sasjs testing passed!"
 else
-    curl -X POST --header "Content-Type:application/json" --data '{"username":"GitHub CI - Adapter SASJS-TESTS (FAIL)", "content":"Automated sasjs-tests failed on the @sasjs/adapter PR on following link.\n'$2'", "avatar_url":"https://i.ibb.co/Lpk7Xvq/error-outline.png"}' $1
-
+    curl -XPOST -d '{"msgtype":"m.text", "body":"content":"Automated sasjs-tests failed on the @sasjs/adapter PR on following link.\n'$2'"}' https://matrix.4gl.io/_matrix/client/r0/rooms/%21BDUPBPEGVvRLKLQUxY:4gl.io/send/m.room.message?access_token=$1
     echo "Cypress sasjs testing failed!"
     exit 1
 fi
