70b31dcb8f8504420b30205fb3fa0590f1da2334
Sabir Hassan <sabirbk06@gmail.com>
2022-10-18 19:10:46 +0500

    fix: export SasjsRequestClient from main index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from './types'
 export * from './types/errors'
 export * from './SASViyaApiClient'
 export * from './SAS9ApiClient'
+export * from './SASjsApiClient'
+export * from './request/SasjsRequestClient'
 export default SASjs
