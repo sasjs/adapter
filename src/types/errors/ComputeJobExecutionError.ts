afff422333f4d8963c0c17be90d4636f7c27e1eb
Stephan Markiefka <smarkiefka@googlemail.com>
2025-09-18 11:57:09 +0200

    feat: Update dependency axios to version 1.12.2

--- src/types/errors/ComputeJobExecutionError.ts
+++ src/types/errors/ComputeJobExecutionError.ts
@@ -1,7 +1,10 @@
 import { Job } from '../Job'
 
 export class ComputeJobExecutionError extends Error {
-  constructor(public job: Job, public log: string) {
+  constructor(
+    public job: Job,
+    public log: string
+  ) {
     super('Error: Job execution failed')
     this.name = 'ComputeJobExecutionError'
     Object.setPrototypeOf(this, ComputeJobExecutionError.prototype)
